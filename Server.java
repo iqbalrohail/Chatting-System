6df45944153b69883bcfd74da147db48ca9163bb
Rohail <iqbalrohail398@gmail.com>
2021-06-17 10:41:35 +0000

    created java classes for first push

---
+++ Server.java
@@ -0,0 +1,17 @@
+import java.net.*;
+
+public class Server {
+
+    ServerSocket serverSocket;
+    Socket socket;
+
+    Server()
+    {
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
